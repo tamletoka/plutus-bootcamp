fb6384a5043aa8d76a9764a1f1266c29bbaf0fc7
Tam Le <tam.le@traveloka.com>
2021-10-05 16:50:14 +0700

    Initial commit

---
+++ src/main/kotlin/com/plutus/plutusbootcamp/PlutusBootcampApplication.kt
@@ -0,0 +1,11 @@
+package com.plutus.plutusbootcamp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlutusBootcampApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlutusBootcampApplication>(*args)
+}
---
+++ src/test/kotlin/com/plutus/plutusbootcamp/PlutusBootcampApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plutus.plutusbootcamp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlutusBootcampApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
